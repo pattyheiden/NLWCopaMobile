80bc5db11c323dcdec0c443c6d4003289fc67a1c
pattyheiden <patriciasilvalimah@gmail.com>
2022-11-10 12:56:43 -0300

    theme customizing

--- App.tsx
+++ App.tsx
@@ -1,27 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, Text, Center } from "native-base";
+import { THEME } from './src/styles/theme';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello World</Text>
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900" >
+        <Text color="white" fontSize={24}>Hello World</Text>
         <StatusBar style="auto" />
-      </View>
+      </Center>
     </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 24,
-  }
-});
+
