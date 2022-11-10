c78a2a40ba83c0c7b3fcf95a1298fe6fadbf6092
pattyheiden <patriciasilvalimah@gmail.com>
2022-11-10 12:42:57 -0300

    NativeBase installation

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+  }
+});
