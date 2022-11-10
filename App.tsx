8baee5b889f3295cae40d0016e390b9d33c3c4cd
pattyheiden <patriciasilvalimah@gmail.com>
2022-11-10 17:53:36 -0300

    fonts, loading and signin

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from "native-base";
+
+import { NativeBaseProvider, Text } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] =  useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });  
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900" >
-        <Text color="white" fontSize={24}>Hello World</Text>
-        <StatusBar style="auto" />
-      </Center>
+      { fontsLoaded ? <SignIn /> : <Loading /> }   
     </NativeBaseProvider>
   );
 }
