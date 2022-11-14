4d252eadf1b984481c7007d0a15be063d31a5088
pattyheiden <patriciasilvalimah@gmail.com>
2022-11-14 17:47:28 -0300

    create button and signin

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,22 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import Logo from '../assets/logo.svg';
+import { Fontisto } from '@expo/vector-icons'
+import { Button } from "../components/Button";
 
-export function SignIn(){
-    return (
-        <Center flex={1} bgColor="gray.900" >
-        <Text color="white" fontSize={24} fontFamily="heading">SignIn</Text>
-      </Center>
-    )
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900" p={7} >
+      <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" /> }
+        type = "SECONDARY"
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}do seu e-mail para criação da sua conta.
+      </Text>
+    </Center>
+  )
 }
