7f610a3c671ae418a72e696067f31968b3f6fd47
pattyheiden <patriciasilvalimah@gmail.com>
2022-11-21 22:19:08 -0300

    navigation

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
-import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 import { AuthContextProvider } from "./src/context/AuthContext";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -16,7 +16,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
