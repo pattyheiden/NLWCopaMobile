148e893af0982beaec7726cdecd26df6b39902a4
pattyheiden <patriciasilvalimah@gmail.com>
2022-11-27 16:43:42 -0300

    environment variables

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -28,7 +28,7 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     const [isUserLoading, setIsUserLoading] = useState(false);
 
     const [request, response, promptAsync] = Google.useAuthRequest({
-        clientId: '683007062034-4527a3ceidui9l8dr212d1u7c10bn6nk.apps.googleusercontent.com',
+        clientId: process.env.CLIENT_ID,
         redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
         scopes: ['profile', 'email']
     })
